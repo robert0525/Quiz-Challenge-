82228cc20034dc5a81a33021a4b02660899d9314
Robert Rydlewski <robertrydlewski1984@gmail.com>
2019-05-25 16:26:42 +0200

    adding secound qestion

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
-
+var correctAnswer = false;
 var qestion1 = prompt("Who was the first President of the United States of America?");
     if ( qestion1.toUpperCase()  === "GEORGE WASHINGTON" ) {
      document.write('<h2>Good job!!!</h2>');
     }
      else {
-     document.write('<h3>  Wrong Answer!!!  it was George Washington </h3>');
+     document.write('<h3>  Wrong Answer!!!  It was George Washington </h3>');
      }
+var qestion2 = prompt("Name the biggest country in the World");
+     if ( qestion2.toLocaleUpperCase () === "RUSSIA") {
+         document.write('<h2>Good job!!!</h2>');
+     }
+     else {
+         document.write("wrong answer");
+     }
+
+     
+        
