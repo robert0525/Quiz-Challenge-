2b47745d2a908e9b6a2ceeb14aa525da4a0f7544
Robert Rydlewski <robertrydlewski1984@gmail.com>
2019-05-25 15:50:35 +0200

    I need to start simple app befre hoping to harder things.. I'll do simple app and latter on I will twick to more advance app lol

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
-window.onload = setTimeout(function(){
-    var qestion1 = prompt("Who was the first President of the United States of America?");
-    if ( qestion1.toUpperCase()  === "GEORGE WASHINGTON" )
+
+var qestion1 = prompt("Who was the first President of the United States of America?");
+    if ( qestion1.toUpperCase()  === "GEORGE WASHINGTON" ) {
      document.write('<h2>Good job!!!</h2>');
-}, 5000);
+    }
+     else {
+     document.write('<h3>  Wrong Answer!!!  it was George Washington </h3>');
+     }
