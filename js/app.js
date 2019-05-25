b4d935ff44e24b4de05aa905e6c7f143f649cf90
Robert Rydlewski <robertrydlewski1984@gmail.com>
2019-05-25 16:38:17 +0200

    5 qestions

--- js/app.js
+++ js/app.js
@@ -13,6 +13,24 @@ var qestion2 = prompt("Name the biggest country in the World");
      else {
          document.write("wrong answer");
      }
-
-     
-        
+var qestion3 = prompt("What\'s the color of the sky?");
+     if ( qestion3.toLocaleUpperCase () === "BLUE") {
+         document.write('<h2>Good job!!!</h2>');
+     }
+     else {
+         document.write("wrong answer");
+     }
+var qestion4 = prompt("The color of the snow ?");
+     if ( qestion4.toLocaleUpperCase () === "WHITE") {
+         document.write('<h2>Good job!!!</h2>');
+     }
+     else {
+         document.write("wrong answer");
+     }
+var qestion5 = prompt("The King of Animals");
+     if ( qestion5.toLocaleUpperCase () === "LION") {
+         document.write('<h2>Good job!!!</h2>');
+     }
+     else {
+         document.write("wrong answer");
+     }
