a0931dbb57c1da354bed3ce90af38a477a6414a2
Robert Rydlewski <robertrydlewski1984@gmail.com>
2019-05-25 15:32:50 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,5 @@
+window.onload = setTimeout(function(){
+    var qestion1 = prompt("Who was the first President of the United States of America?");
+    if ( qestion1.toUpperCase()  === "GEORGE WASHINGTON" )
+     document.write('<h2>Good job!!!</h2>');
+}, 5000);
